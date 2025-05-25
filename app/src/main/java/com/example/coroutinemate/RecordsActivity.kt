009ac0de998afc9bd1c5255ed9e6b991d18587c6
yeapning <ybys1004@naver.com>
2025-05-25 19:33:29 +0900

    feat:하단 네비게이션 바 + Activity 3개 골조 구현 - BottomNavigation 구현 - 네비게이션 고정 처리 - 화면 전환 로직 구현 - 선택 효과 구현

--- app/src/main/java/com/example/coroutinemate/RecordsActivity.kt
+++ app/src/main/java/com/example/coroutinemate/RecordsActivity.kt
@@ -7,15 +7,23 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class RecordsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_records)
 
         val nav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
         nav.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_home -> true // 현재 화면
+                R.id.nav_home -> {
+                    if (this::class != MainActivity::class) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        overridePendingTransition(0, 0)
+                        finish()
+                    }
+                    true
+                }
                 R.id.nav_workout -> {
                     if (this::class != WorkoutActivity::class) {
                         startActivity(Intent(this, WorkoutActivity::class.java))
@@ -24,17 +32,10 @@ class MainActivity : AppCompatActivity() {
                     }
                     true
                 }
-                R.id.nav_records -> {
-                    if (this::class != RecordsActivity::class) {
-                        startActivity(Intent(this, RecordsActivity::class.java))
-                        overridePendingTransition(0, 0)
-                        finish()
-                    }
-                    true
-                }
+                R.id.nav_records -> true // 현재 화면
                 else -> false
             }
         }
-        nav.selectedItemId = R.id.nav_home
+        nav.selectedItemId = R.id.nav_records
     }
 }
